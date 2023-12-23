15f9e1e92cbddc2886cdae778e80f86fa14053af
cream <837800910@qq.com>
2023-12-23 16:29:08 +0800

    创建springboot工程

---
+++ src/main/java/com/cream/helper/TestHelperApplication.java
@@ -0,0 +1,13 @@
+package com.cream.helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestHelperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cream/helper/TestHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cream.helper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
