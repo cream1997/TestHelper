824210b84cf43cc7852422afca41670ca6bd2463
cream <837800910@qq.com>
2023-12-29 01:26:31 +0800

    代码微调

--- front-app/src/App.vue
+++ front-app/src/App.vue
@@ -1,10 +1,7 @@
 <script setup lang="ts">
 import {RouterView, useRouter} from "vue-router";
-import {onMounted} from "vue";
 // 跳转到登录页
-onMounted(() => {
-  useRouter().push("/login")
-})
+useRouter().push("/login")
 </script>
 
 <template>
