bca3870e2bd5581f6acb1b5c6251a2ceec7ea7db
cream <837800910@qq.com>
2023-12-30 06:55:01 +0800

    账户实体类修改

--- src/main/java/com/cream/helper/obj/entity/account/Account.java
+++ src/main/java/com/cream/helper/obj/entity/account/Account.java
@@ -1,19 +1,39 @@
 package com.cream.helper.obj.entity.account;
 
+import com.baomidou.mybatisplus.annotation.*;
 import lombok.Getter;
 
+@Getter
+@TableName("t_account")
 public class Account {
-    @Getter
-    private final long id;
-    @Getter
+    // 雪花算法生成id
+    @TableId(type = IdType.ASSIGN_ID)
+    @TableField("id")
+    private long id;
+
+    @TableField("username")
     private final String username;
-    @Getter
+
+    @TableField("password")
     private final String password;
 
+    /**
+     * 创建时间
+     */
+    @TableField(value = "create_time")
+    private final long createTime;
 
-    public Account(long id, String username, String password) {
-        this.id = id;
+    /**
+     * 逻辑删除
+     */
+    @TableLogic
+    @TableField("deleted")
+    private boolean deleted;
+
+
+    public Account(String username, String password, long createTime) {
         this.username = username;
         this.password = password;
+        this.createTime = createTime;
     }
 }
