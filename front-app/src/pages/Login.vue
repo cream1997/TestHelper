b4886273f2e098f53343a29b5bd5c896eff53281
cream <837800910@qq.com>
2023-12-30 01:07:55 +0800

    登录页重绘

--- front-app/src/pages/Login.vue
+++ front-app/src/pages/Login.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts" name="Login">
-import {ref} from "vue";
+import {onMounted, onUnmounted, ref} from "vue";
 import {useRouter} from "vue-router";
 import axios from "@/axios/axios";
 
@@ -13,7 +13,19 @@ if (account) {
   // todo 默认账号直接登录
 }
 
+function accountInvalid() {
+  if (!username.value || !password.value) {
+    alert("账号密码不能为空")
+    return true;
+  } else {
+    return false;
+  }
+}
+
 function login() {
+  if (accountInvalid()) {
+    return
+  }
   router.push({
     path: "/home"
   })
@@ -21,6 +33,9 @@ function login() {
 
 // todo
 function register() {
+  if (accountInvalid()) {
+    return;
+  }
   axios.post("/register",
       {
         username: username.value,
@@ -37,73 +52,127 @@ function defaultAccount() {
   // todo 后续需要修改（可能根据本地存储）
   return undefined;
 }
+
+function setLoginPageStyle() {
+  onMounted(() => {
+    document.querySelector("body")
+        ?.setAttribute("style",
+            `background: linear-gradient(to right, #65CBF7, #B3A5FC);
+                   width: 100vw;
+                   height: 100vh;
+                   margin: 0;`)
+  })
+  onUnmounted(() => {
+    document.querySelector("body")
+        ?.removeAttribute("style")
+  })
+}
+
+setLoginPageStyle();
 </script>
 
 <template>
-  <div id="container">
-    <h1>测试助手</h1>
-    <div class="form-box">
-      <div class="item">
-        <i class="fa fa-github-alt" style="font-size:24px"></i>
-        <input type="text" v-model="username" placeholder="请输入用户名" required><br>
-      </div>
-      <div class="item">
-        <i class="fa fa-search" style="font-size:24px"></i>
-        <input type="password" v-model="password" placeholder="请输入密码" required><br>
-      </div>
+  <div class="box">
+    <div class="left">
+      <img src="../assets/flower.jpg" alt="loading">
     </div>
-    <button @click="login">登录</button>
-    <button @click="register">注册</button>
+    <div class="right">
+      <h1>测试助手</h1>
+      <input type="text" v-model="username" class="inputItem" placeholder="请输入账号">
+      <input type="password" v-model="password" class="inputItem" placeholder="请输入密码">
+      <a href="#" class="forgetPassword">忘记密码?</a>
+      <button class="btn" @click="login">登录</button>
+      <button class="btn" @click="register">注册</button>
+    </div>
   </div>
 </template>
 
 <style scoped>
 body {
-  background: url('../image/2.jpg') no-repeat;
-  background-size: 100% auto;
+  background: linear-gradient(to right, #65CBF7, #B3A5FC);
+  width: 100vw;
+  height: 100vh;
+  margin: 0;
 }
 
-#container {
-  width: 30%;
-  height: auto;
-  margin: 13% auto 0;
+.box {
+  width: 60%;
+  height: 450px;
+  box-shadow: 0 5px 15px rgba(0, 0, 0, .8);
+  display: flex;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.left {
+  width: 65%;
+}
+
+.left > img {
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.right {
+  width: 35%;
+  height: 100%;
+  background-color: #fff;
+  padding: 0 20px;
+}
+
+h1 {
   text-align: center;
-  background: #00000060;
-  padding: 20px 50px;
+  padding-top: 45px;
+  margin-top: 0;
 }
 
-#container-box h1 {
-  color: #fff;
-}
-
-#container .form-box .item {
-  margin-top: 15px;
-}
-
-#container .form-box .item i {
+.inputItem {
+  height: 44px;
+  padding: 0;
+  padding-left: 5px;
+  border: none;
+  background: none;
+  outline: none;
+  border-bottom: 3px solid #B3A5FC;
+  width: 100%;
   font-size: 18px;
-  color: #fff;
-}
-
-#container .form-box .item input {
-  width: 180px;
-  font-size: 18px;
-  border: 0;
-  border-bottom: 2px solid #fff;
-  padding: 5px 10px;
-  background: #ffffff00;
-  color: #fff;
-}
-
-#container button {
   margin-top: 20px;
-  width: 190px;
-  height: 30px;
-  font-size: 20px;
-  font-weight: 700;
-  color: #fff;
-  background-image: linear-gradient(to right, #74ebd5 0%, #9face6 100%);
-  border: 0;
-  border-radius: 15px;
+}
+
+.forgetPassword {
+  margin-top: 25px;
+  color: #9C3493;
+  text-align: right;
+  display: block;
+}
+
+.btn {
+  background: linear-gradient(to right, #65CBF7, #B3A5FC);
+  color: #9C3493;
+  font-weight: bold;
+  border: none;
+  border-radius: 30px;
+  height: 46px;
+  width: 80%;
+  font-size: 18px;
+  display: block;
+  margin: auto;
+  margin-top: 10px;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background: linear-gradient(to right, #B3A5FC, #65CBF7);
+}
+
+@media screen and(min-width: 960px) {
+  .box {
+    max-width: 950px;
+    min-width: 750px;
+  }
 }
 </style>
