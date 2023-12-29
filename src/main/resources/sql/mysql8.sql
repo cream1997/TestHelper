ff61478df4c71d5b99963da00668321147c7cf2c
cream <837800910@qq.com>
2023-12-30 06:53:54 +0800

    账户表更新

--- src/main/resources/sql/mysql8.sql
+++ src/main/resources/sql/mysql8.sql
@@ -1,8 +1,11 @@
-CREATE TABLE t_account
+-- 账户表
+CREATE TABLE `t_account`
 (
-    `id`       bigint      NOT NULL,
-    `username` varchar(50) NOT NULL,
-    `password` varchar(25) NOT NULL,
+    `id`          bigint      NOT NULL,
+    `username`    varchar(50) NOT NULL,
+    `password`    varchar(25) NOT NULL,
+    `create_time` date        NOT NULL,
+    `deleted`     tinyint(1) NOT NULL,
     PRIMARY KEY (`id`),
     UNIQUE KEY `account_un` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
