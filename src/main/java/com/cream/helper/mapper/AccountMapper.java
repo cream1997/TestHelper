d5dd6fb6bcee54aae90abc73b6e19bb7c77ecccc
cream <837800910@qq.com>
2023-12-30 17:30:16 +0800

    修改创建账号接口

--- src/main/java/com/cream/helper/mapper/AccountMapper.java
+++ src/main/java/com/cream/helper/mapper/AccountMapper.java
@@ -1,13 +1,9 @@
 package com.cream.helper.mapper;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.cream.helper.obj.entity.account.Account;
 import org.apache.ibatis.annotations.Param;
 
-public interface AccountMapper {
-
+public interface AccountMapper extends BaseMapper<Account> {
     Account getAccount(@Param("username") String username);
-
-    void addAccount(@Param("id") long id,
-                    @Param("username") String username,
-                    @Param("password") String password);
 }
--- src/main/java/com/cream/helper/service/AccountService.java
+++ src/main/java/com/cream/helper/service/AccountService.java
@@ -1,8 +1,9 @@
 package com.cream.helper.service;
 
-import cn.hutool.core.util.IdUtil;
 import com.cream.helper.mapper.AccountMapper;
+import com.cream.helper.obj.entity.account.Account;
 import com.cream.helper.pojo.Result;
+import com.cream.helper.utils.Times;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,7 +26,7 @@ public class AccountService {
             return Result.fail(null, "用户名或密码不能为空");
         }
         try {
-            accountMapper.addAccount(IdUtil.getSnowflakeNextId(), username, password);
+            accountMapper.insert(new Account(username, password, Times.now()));
         } catch (Exception e) {
             return Result.fail(e, "注册失败");
         }
