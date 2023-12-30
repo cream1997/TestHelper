4415e6ffc5bc6f9f388bcf537e6f8c98b30ae2fa
cream <837800910@qq.com>
2023-12-30 23:32:30 +0800

    引入element plus样式

--- front-app/src/main.ts
+++ front-app/src/main.ts
@@ -4,13 +4,14 @@ import {createApp} from 'vue'
 import App from './App.vue'
 import router from "@/router";
 import ElementPlus from "element-plus"
+import 'element-plus/dist/index.css'
 
 createApp(App)
     .use(router)
     // size 用于设置表单组件的默认尺寸，zIndex 用于设置弹出组件的层级，zIndex 的默认值为 2000
     .use(ElementPlus, {
         size: "small",
-        zIndex: 3000
+        zIndex: 2000
     })
     .mount('#app')
 
