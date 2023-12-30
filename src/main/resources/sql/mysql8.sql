f943cca025beba3b16ecaab7feffdabcd0edf751
cream <837800910@qq.com>
2023-12-30 16:29:17 +0800

    sql修改

--- src/main/resources/sql/mysql8.sql
+++ src/main/resources/sql/mysql8.sql
@@ -4,8 +4,9 @@ CREATE TABLE `t_account`
     `id`          bigint      NOT NULL,
     `username`    varchar(50) NOT NULL,
     `password`    varchar(25) NOT NULL,
-    `create_time` date        NOT NULL,
     `deleted`     tinyint(1) NOT NULL,
+    `create_time` datetime    NOT NULL,
+    `update_time` datetime    NOT NULL,
     PRIMARY KEY (`id`),
     UNIQUE KEY `account_un` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
