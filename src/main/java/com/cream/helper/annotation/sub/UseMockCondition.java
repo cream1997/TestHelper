e7d6dd2e327f02ebfc5d06d416e3a88585a61c4c
cream <837800910@qq.com>
2024-01-01 22:29:49 +0800

    system.err打印改为使用日志框架

--- src/main/java/com/cream/helper/annotation/sub/UseMockCondition.java
+++ src/main/java/com/cream/helper/annotation/sub/UseMockCondition.java
@@ -1,18 +1,19 @@
 package com.cream.helper.annotation.sub;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.core.env.Environment;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 
+@Slf4j
 public class UseMockCondition implements Condition {
     @Override
     public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
         Environment environment = context.getEnvironment();
         Boolean useMock = environment.getProperty("project.use-mock-component", Boolean.class);
         if (Boolean.TRUE.equals(useMock)) {
-            // todo 替换为使用日志框架
-            System.err.println("项目开启了mock组件");
+            log.error("项目开启了mock组件");
             return true;
         } else {
             return false;
