49a7ba59c47f33252498b55e503b24f34d1783e6
cream <837800910@qq.com>
2024-01-02 02:43:06 +0800

    去除默认样式

--- front-app/src/App.vue
+++ front-app/src/App.vue
@@ -7,3 +7,30 @@ useRouter().push("/login")
 <template>
   <RouterView/>
 </template>
+
+<style>
+/* 设置根元素的字体和背景颜色 */
+html, body {
+  font-family: sans-serif;
+  font-size: 16px;
+  background-color: #f5f5f5;
+  margin: 0;
+  padding: 0;
+}
+
+/* 重置所有元素的内外边距以及边框 */
+#app {
+  margin: 0;
+  padding: 0;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  outline: none;
+  border: none;
+}
+
+
+</style>
