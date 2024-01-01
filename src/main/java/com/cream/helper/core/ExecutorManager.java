354c82e6eab82ba54f3904ed7103e71182800b03
cream <837800910@qq.com>
2024-01-01 23:32:28 +0800

    定义通用线程池

--- src/main/java/com/cream/helper/core/ExecutorManager.java
+++ src/main/java/com/cream/helper/core/ExecutorManager.java
@@ -2,16 +2,25 @@ package com.cream.helper.core;
 
 import org.springframework.stereotype.Component;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Component
 public class ExecutorManager {
 
     private final AtomicInteger scheduledThreadNum = new AtomicInteger(0);
+    private final AtomicInteger commonThreadNum = new AtomicInteger(0);
+
+
+    /**
+     * 通用线程池
+     */
+    private final ExecutorService COMMON_THREAD_POLL = Executors.newCachedThreadPool(
+            r -> new Thread(r, "TestHelper Common thread - " + commonThreadNum.getAndIncrement()));
+
+    /**
+     * 定时任务线程池
+     */
     private final ScheduledExecutorService scheduledExecutor = Executors.newScheduledThreadPool(2,
             r -> new Thread(r, "TestHelper定时器线程: " + scheduledThreadNum.getAndIncrement()));
 
@@ -26,5 +35,13 @@ public class ExecutorManager {
 
     public ScheduledFuture<?> runFixedDelay(Runnable task, long initialDelay, long period, TimeUnit unit) {
         return scheduledExecutor.scheduleWithFixedDelay(task, initialDelay, period, unit);
+    }
+
+    public <T> Future<T> submit(Callable<T> task) {
+        return COMMON_THREAD_POLL.submit(task);
+    }
+
+    public void execute(Runnable runnable) {
+        COMMON_THREAD_POLL.execute(runnable);
     }
 }
