446e127ae4e5aa26ba1e863e6564fb33f9daf22d
cream <837800910@qq.com>
2024-01-02 02:25:50 +0800

    feat: 定义两个Account组件中的路由组件

---
+++ front-app/src/pages/Role.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts" name="Role">
+
+</script>
+
+<template>
+  角色模块
+</template>
+
+<style scoped>
+
+</style>
---
+++ front-app/src/pages/User.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts" name="User">
+
+</script>
+
+<template>
+  User模块
+</template>
+
+<style scoped>
+
+</style>
