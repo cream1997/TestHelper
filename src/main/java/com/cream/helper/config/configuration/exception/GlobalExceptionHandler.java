db4a6ea21f85bba645824ee0fd4f178136460f00
cream <837800910@qq.com>
2024-01-14 23:27:54 +0800

    后端全局异常处理器添加@ResponseBody注解

--- src/main/java/com/cream/helper/config/configuration/exception/GlobalExceptionHandler.java
+++ src/main/java/com/cream/helper/config/configuration/exception/GlobalExceptionHandler.java
@@ -3,8 +3,10 @@ package com.cream.helper.config.configuration.exception;
 import com.cream.helper.obj.Ret;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @ControllerAdvice
+@ResponseBody
 public class GlobalExceptionHandler {
     @ExceptionHandler(CommonException.class)
     public Ret<String> handleException(CommonException e) {
