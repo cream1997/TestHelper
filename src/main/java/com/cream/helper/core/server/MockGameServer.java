b021b1eded95e699be45bfe8d878aab23ac109c7
cream <837800910@qq.com>
2024-01-20 08:41:37 +0800

    对MockGameServer的关闭做异步处理，否则会阻塞程序启动

--- src/main/java/com/cream/helper/core/server/MockGameServer.java
+++ src/main/java/com/cream/helper/core/server/MockGameServer.java
@@ -2,10 +2,9 @@ package com.cream.helper.core.server;
 
 import com.cream.helper.config.ProjectConfig;
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.ChannelFuture;
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
-import io.netty.channel.ChannelPipeline;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
@@ -27,27 +26,35 @@ public class MockGameServer {
     public MockGameServer(ProjectConfig projectConfig) {
         NioEventLoopGroup boss = new NioEventLoopGroup(BossThreads);
         NioEventLoopGroup worker = new NioEventLoopGroup(WorkerThreads);
+        Channel serverChannel = null;
         try {
-            ServerBootstrap serverBootstrap = new ServerBootstrap();
-            serverBootstrap.group(boss, worker)
+            serverChannel = new ServerBootstrap()
+                    .group(boss, worker)
                     .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 128) //设置最大连接数量
+                    .childOption(ChannelOption.SO_KEEPALIVE, true) //设置channel保持连接
                     .childHandler(new ChannelInitializer<SocketChannel>() {
                         @Override
                         protected void initChannel(SocketChannel ch) {
-                            ChannelPipeline pipeline = ch.pipeline();
-                            // todo 添加自定义处理器
-                            pipeline.addLast(null);
+                            ch.pipeline()
+                                    .addLast(null);
                         }
                     })
-                    .option(ChannelOption.SO_BACKLOG, 128)
-                    .childOption(ChannelOption.SO_KEEPALIVE, true);
-            ChannelFuture future = serverBootstrap.bind(projectConfig.getMockServerPort()).sync();
-            future.channel().closeFuture().sync();
+                    .bind(projectConfig.getMockServerPort())
+                    .sync()
+                    .channel();
         } catch (Exception e) {
             log.error("MockGameServer初始化异常", e);
         } finally {
-            boss.shutdownGracefully();
-            worker.shutdownGracefully();
+            if (serverChannel != null) {
+                serverChannel.closeFuture()
+                        .addListener(future -> shutdownGracefully(boss, worker));
+            }
         }
+    }
+
+    private void shutdownGracefully(NioEventLoopGroup boss, NioEventLoopGroup worker) {
+        boss.shutdownGracefully();
+        worker.shutdownGracefully();
     }
 }
