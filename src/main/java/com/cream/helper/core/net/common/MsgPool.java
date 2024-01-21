ff3e74d9196b6ea9ad756825319fb14faf80391c
cream <837800910@qq.com>
2024-01-21 18:17:28 +0800

    取消息加锁，保证取指定消息时，不会被fetchAll抢先

--- src/main/java/com/cream/helper/core/net/common/MsgPool.java
+++ src/main/java/com/cream/helper/core/net/common/MsgPool.java
@@ -1,6 +1,8 @@
 package com.cream.helper.core.net.common;
 
+import com.cream.helper.config.configuration.exception.CommonError;
 import com.cream.helper.core.net.common.msg.base.Message;
+import com.cream.helper.utils.Times;
 import com.google.protobuf.GeneratedMessageV3;
 import lombok.extern.slf4j.Slf4j;
 
@@ -9,11 +11,17 @@ import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
 
 @Slf4j
 public class MsgPool {
 
     private static final int CAPACITY = 500;
+    private static final long TIMEOUT_MS = 1500;
+    /**
+     * 取消息加锁，保证取指定消息时，不会被fetchAll抢先
+     */
+    private final ReentrantLock fetchLock = new ReentrantLock();
 
     /**
      * 玩家的响应消息队列, 先进先出的单向队列
@@ -25,7 +33,7 @@ public class MsgPool {
         // 消息添加到队列尾部
         try {
             // 将元素插入队列尾部，等待指定时间，如果队列仍然满，则返回 false。
-            if (!resMsgQueue.offer(message, 2, TimeUnit.SECONDS)) {
+            if (!resMsgQueue.offer(message, TIMEOUT_MS, TimeUnit.MILLISECONDS)) {
                 log.error("放入消息时，队列已满，请检查系统");
             }
         } catch (InterruptedException e) {
@@ -38,30 +46,41 @@ public class MsgPool {
             return Collections.emptyList();
         }
         List<Message<? extends GeneratedMessageV3>> result = new ArrayList<>();
-        while (!resMsgQueue.isEmpty()) {
-            Message<? extends GeneratedMessageV3> message = resMsgQueue.poll();
-            if (message == null) {
-                log.error("取出消息为空，请检查");
-            } else {
-                result.add(message);
+        fetchLock.lock();
+        try {
+            while (!resMsgQueue.isEmpty()) {
+                Message<? extends GeneratedMessageV3> message = resMsgQueue.poll();
+                if (message == null) {
+                    log.error("取出消息为空，请检查");
+                } else {
+                    result.add(message);
+                }
             }
+        } finally {
+            fetchLock.unlock();
         }
         return result;
     }
 
-    public <T extends Message<?>> T fetchResMsg(Class<T> msgClass) {
-        T result = null;
-        while (result == null) {
-            for (Message<? extends GeneratedMessageV3> message : resMsgQueue) {
-                if (msgClass.isInstance(message)) {
-                    result = msgClass.cast(message);
-                    break;
+    public <T extends Message<?>> T fetchResMsg(Class<T> msgClass) throws CommonError {
+        long startTime = Times.now();
+        fetchLock.lock();
+        try {
+            for (; ; ) {
+                if (Times.now() - startTime > TIMEOUT_MS) {
+                    throw new CommonError("从队列中取出消息超时，请检查");
+                }
+                for (Message<? extends GeneratedMessageV3> message : resMsgQueue) {
+                    if (msgClass.isInstance(message)) {
+                        if (!resMsgQueue.remove(message)) {
+                            log.error("从队列中移除消息失败，请检查");
+                        }
+                        return msgClass.cast(message);
+                    }
                 }
             }
+        } finally {
+            fetchLock.unlock();
         }
-        if (!resMsgQueue.remove(result)) {
-            log.error("从队列中移除消息失败，请检查");
-        }
-        return result;
     }
 }
