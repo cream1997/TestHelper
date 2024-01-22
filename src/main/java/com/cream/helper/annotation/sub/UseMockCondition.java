3e411f5d86e08a1083a7d41fd42c7f5d4ee53f35
cream <837800910@qq.com>
2024-01-22 18:45:52 +0800

    fix：日志提示重复

--- src/main/java/com/cream/helper/annotation/sub/UseMockCondition.java
+++ src/main/java/com/cream/helper/annotation/sub/UseMockCondition.java
@@ -6,14 +6,24 @@ import org.springframework.context.annotation.ConditionContext;
 import org.springframework.core.env.Environment;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Slf4j
 public class UseMockCondition implements Condition {
+
+    private final static Set<String> alreadyLogged = new HashSet<>();
+
     @Override
     public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
         Environment environment = context.getEnvironment();
         Boolean useMock = environment.getProperty("app-setup.use-mock-component", Boolean.class);
         if (Boolean.TRUE.equals(useMock)) {
-            log.warn("项目开启了mock组件{}", metadata);
+            String componentName = metadata.toString();
+            if (!alreadyLogged.contains(componentName)) {
+                log.warn("项目开启了mock组件：{}", componentName);
+                alreadyLogged.add(componentName);
+            }
             return true;
         } else {
             return false;
