85e2a4af1a08c709235b43ca66da89690bebf2d8
cream <837800910@qq.com>
2024-02-09 02:06:17 +0800

    自定义List、Map

---
+++ src/main/java/com/cream/helper/obj/common/List.java
@@ -0,0 +1,9 @@
+package com.cream.helper.obj.common;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+
+@Slf4j
+public class List<E> extends ArrayList<E> {
+}
---
+++ src/main/java/com/cream/helper/obj/common/Map.java
@@ -0,0 +1,9 @@
+package com.cream.helper.obj.common;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashMap;
+
+@Slf4j
+public class Map<K, V> extends HashMap<K, V> {
+}
