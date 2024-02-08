951d89fed25b8708ded8dcc75d0daa288fc00e29
cream <837800910@qq.com>
2024-02-09 02:14:32 +0800

    自定义List、Map

--- src/main/java/com/cream/helper/obj/common/List.java
+++ src/main/java/com/cream/helper/obj/common/List.java
@@ -1,9 +1,26 @@
 package com.cream.helper.obj.common;
 
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Slf4j
 public class List<E> extends ArrayList<E> {
+
+    private static final AtomicLong IDX = new AtomicLong(0);
+
+    @Getter
+    private final long id;
+
+    public List() {
+        this.id = IDX.incrementAndGet();
+    }
+
+    public List(Collection<? extends E> c) {
+        super(c);
+        this.id = IDX.incrementAndGet();
+    }
 }
--- src/main/java/com/cream/helper/obj/common/Map.java
+++ src/main/java/com/cream/helper/obj/common/Map.java
@@ -1,9 +1,24 @@
 package com.cream.helper.obj.common;
 
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Slf4j
 public class Map<K, V> extends HashMap<K, V> {
+    private static final AtomicLong IDX = new AtomicLong(0);
+
+    @Getter
+    private final long id;
+
+    public Map() {
+        this.id = IDX.incrementAndGet();
+    }
+
+    public Map(java.util.Map<? extends K, ? extends V> m) {
+        super(m);
+        this.id = IDX.incrementAndGet();
+    }
 }
