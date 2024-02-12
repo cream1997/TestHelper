cf141ceaed6feae9afe45aa21d7a05871fafd4f1
cream <837800910@qq.com>
2024-02-12 22:14:48 +0800

    =====包管理工具升级->pnpm

--- front-app/vite.config.ts
+++ front-app/vite.config.ts
@@ -1,17 +1,16 @@
-import {fileURLToPath, URL} from 'node:url'
+import { fileURLToPath, URL } from 'node:url'
 
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import VueSetupExtend from "vite-plugin-vue-setup-extend"
+
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        vue(),
-        VueSetupExtend()
-    ],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  }
 })
