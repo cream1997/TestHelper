afd2a29a98f8480e54cd61ba8949a3803e548f1c
cream <837800910@qq.com>
2024-03-31 22:40:45 +0800

    build报错

--- front-app/src/interface/store/msg/MsgFilterSettingDTO.ts
+++ front-app/src/interface/store/msg/MsgFilterSettingDTO.ts
@@ -1,3 +1,5 @@
+import type MsgFilterSettingVO from "@/interface/vo/msg/filter/MsgFilterSettingVO";
+
 export default interface MsgFilterSettingDTO {
   defaultSetting: MsgFilterSettingVO;
   customSetting: MsgFilterSettingVO;
